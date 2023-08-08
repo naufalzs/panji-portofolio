63f6695a7d31bad49f30f1e30c8c96ce415ce44f
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-08-08 10:07:05 +0700

    add link wrapper into button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-export default function Button({ color, onClick, children }) {
+const Wrapper = ({ href, children }) =>
+  href ? <a href={href} target="_blank" rel="noreferrer">{children}</a> : <>{children}</>;
+
+export default function Button({ color, href, onClick, children }) {
   const bgColor = color === "secondary" ? "bg-secondary" : "bg-primary";
+
   return (
-    <button
-      className={`capitalize py-4 px-5 rounded-lg  ${bgColor} text-lg leading-none border-2 border-neutral-100 shadow-btn hover:shadow-none hover:translate-x-1 hover:translate-y-1 transition-all`}
-      onClick={onClick ? () => onClick() : undefined}
-    >
-      {children}
-    </button>
+    <Wrapper href={href}>
+      <button
+        className={`capitalize py-4 px-5 rounded-lg  ${bgColor} text-lg leading-none border-2 border-neutral-100 shadow-btn hover:shadow-none hover:translate-x-1 hover:translate-y-1 transition-all`}
+        onClick={onClick ? () => onClick() : undefined}
+      >
+        {children}
+      </button>
+    </Wrapper>
   );
 }
