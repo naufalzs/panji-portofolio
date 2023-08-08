cd66eaa939964414e6dbff47f5e2fd79b791435d
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-08-08 10:31:51 +0700

    feat: add gray color into button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,37 @@
 import React from "react";
 
 const Wrapper = ({ href, children }) =>
-  href ? <a href={href} target="_blank" rel="noreferrer">{children}</a> : <>{children}</>;
+  href ? (
+    <a href={href} target="_blank" rel="noreferrer">
+      {children}
+    </a>
+  ) : (
+    <>{children}</>
+  );
 
-export default function Button({ color, href, onClick, children }) {
-  const bgColor = color === "secondary" ? "bg-secondary" : "bg-primary";
+export default function Button({ color, href, onClick, disabled, children }) {
+  const colorClass = () => {
+    switch (color) {
+      case "secondary":
+        return "bg-secondary";
+
+      case "gray":
+        return "bg-slate-400";
+
+      default:
+        return "bg-primary";
+    }
+  };
+
+  const bgColor = colorClass();
+  const hoverClass = !disabled
+    ? "hover:shadow-none hover:translate-x-1 hover:translate-y-1 transition-all"
+    : "";
 
   return (
     <Wrapper href={href}>
       <button
-        className={`capitalize py-4 px-5 rounded-lg  ${bgColor} text-lg leading-none border-2 border-neutral-100 shadow-btn hover:shadow-none hover:translate-x-1 hover:translate-y-1 transition-all`}
+        className={`capitalize py-4 px-5 rounded-lg  ${bgColor} text-lg leading-none border-2 border-neutral-100 shadow-mini ${hoverClass}`}
         onClick={onClick ? () => onClick() : undefined}
       >
         {children}
